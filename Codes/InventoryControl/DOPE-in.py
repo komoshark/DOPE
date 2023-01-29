9039ae8c2823d732205810015e477126ee30746e
komo <komoshark@gmail.com>
2023-01-29 02:47:57 -0600

    add comment

--- Codes/InventoryControl/DOPE-in.py
+++ Codes/InventoryControl/DOPE-in.py
@@ -10,10 +10,6 @@ import pickle
 import sys
 import random
 
-
-start_time = time.time()
-
-
 # temp = sys.argv[1:]
 # RUN_NUMBER = int(temp[0])
 
@@ -24,17 +20,18 @@ np.random.seed(RUN_NUMBER)
 
 #RUN_NUMBER = 0
 
-#Initialize:
+#Initialize:(Better with error handling like 'with' statement in 'open' operation)
 f = open('model-in.pckl', 'rb')
 [NUMBER_SIMULATIONS, NUMBER_EPISODES, P, R, C, CONSTRAINT, N_STATES, actions, EPISODE_LENGTH, DELTA] = pickle.load(f)
 f.close()
-
-
+print(NUMBER_EPISODES)
+#read optimal policy from pre-trained model
 f = open('solution-in.pckl', 'rb')
 [opt_policy_con, opt_value_LP_con, opt_cost_LP_con, opt_q_con, opt_policy_uncon, opt_value_LP_uncon, opt_cost_LP_uncon, opt_q_uncon] = pickle.load(f)
 f.close()
 
 
+#read safe baseline policy from pre-trained model
 f = open('base-in.pckl', 'rb')
 [pi_b, val_b, cost_b, q_b] = pickle.load(f)
 f.close()
@@ -45,13 +42,16 @@ M = 1024* N_STATES*EPISODE_LENGTH**2/EPS**2
 
 #np.random.seed(RUN_NUMBER)
 
+#maximum permissible value for safe baseline policy
 Cb = cost_b[0, 0]
 
 print(CONSTRAINT - Cb)
 
+#K0, number of episdoes that use safe baseline policy firstly for no solution concern
 K0 = N_STATES**3*EPISODE_LENGTH**3/((CONSTRAINT - Cb)**2) 
+print('K0: ', K0)
 
-NUMBER_EPISODES = int(NUMBER_EPISODES)
+NUMBER_EPISODES = int(NUMBER_EPISODES) #ensure data type
 NUMBER_SIMULATIONS = int(NUMBER_SIMULATIONS)
 
 STATES = np.arange(N_STATES)
@@ -61,30 +61,49 @@ ConRegret2 = np.zeros((NUMBER_SIMULATIONS,NUMBER_EPISODES))
 
 NUMBER_INFEASIBILITIES = np.zeros((NUMBER_SIMULATIONS, NUMBER_EPISODES))
 
-
-L = math.log(6 * N_STATES**2 * EPISODE_LENGTH * NUMBER_EPISODES / DELTA)#math.log(2 * N_STATES * EPISODE_LENGTH * NUMBER_EPISODES * N_STATES**2 / DELTA)
+# !! L not works here, the original below value should be used on formula about Rk and Ck
+L = math.log(2 * N_STATES**2 * EPISODE_LENGTH * NUMBER_EPISODES / DELTA)#math.log(2 * N_STATES * EPISODE_LENGTH * NUMBER_EPISODES * N_STATES**2 / DELTA)
 
 for sim in range(NUMBER_SIMULATIONS):
     util_methods = utils(EPS, DELTA, M, P,R,C,EPISODE_LENGTH,N_STATES,actions,CONSTRAINT,Cb)
-    ep_count = np.zeros((N_STATES, N_STATES))
-    ep_count_p = np.zeros((N_STATES, N_STATES, N_STATES))
+    ep_count = np.zeros((N_STATES, N_STATES)) #I(s,a), I is the indicator function
+    ep_count_p = np.zeros((N_STATES, N_STATES, N_STATES)) #I(s,a,s')
     objs = []
     cons = []
+    start_time = time.time()#record duration
+    is_fixed = False
+    cnt = 0
+    #these variables are used for fixed method
+    pi_f = np.zeros((N_STATES, EPISODE_LENGTH, N_STATES)) #[s,h,a], val_f, cost_f, log, q_f
+    q_f = np.zeros((N_STATES,EPISODE_LENGTH, N_STATES)) #q_policy
+    val_f = np.zeros((N_STATES, EPISODE_LENGTH)) #value
+    cost_f = np.zeros((N_STATES,EPISODE_LENGTH)) #cost
+
     for episode in range(NUMBER_EPISODES):
-        
         if episode <= K0:
+            #use safe baseline policy for first K0 episodes
             pi_k = pi_b
             val_k = val_b
             cost_k = cost_b
             q_k = q_b
-            util_methods.setCounts(ep_count_p, ep_count)
-            util_methods.update_empirical_model(0)
-            util_methods.compute_confidence_intervals(L, 1)
+            util_methods.setCounts(ep_count_p, ep_count) #number of times the pair (s,a) and (s,a,s') was observed
+            util_methods.update_empirical_model(0) #update P_hat according to number of occurances
+            util_methods.compute_confidence_intervals(L, 1) #compute confidence bound beta
         else:
             util_methods.setCounts(ep_count_p, ep_count)
             util_methods.update_empirical_model(0)
             util_methods.compute_confidence_intervals(L, 0)
-            pi_k, val_k, cost_k, log, q_k = util_methods.compute_extended_LP(0, Cb)
+            if is_fixed == False: #update policy every 100 episodes
+                #print('Update Policy on Epoch{}'.format(episode))
+                pi_f, val_f, cost_f, log, q_f = util_methods.compute_extended_LP(0, Cb)
+                is_fixed = True
+            cnt += 1
+            if cnt % 100 == 0:
+                is_fixed = False
+            pi_k = pi_f
+            val_k = val_f
+            cost_k = cost_f
+            q_k = q_f
             if log != 'Optimal':  #Added this part to resolve issues about infeasibility. Because I am not sure about the value of K0, this condition would take care of that
                 # pi_k = pi_b
                 # val_k = val_b
@@ -92,15 +111,14 @@ for sim in range(NUMBER_SIMULATIONS):
                 # q_k = q_b
                 print(log)
 
-
-        
+        #calculate safe objective regret after K learning episodes
         if episode == 0:
-            ObjRegret2[sim, episode] = abs(val_k[0, 0] - opt_value_LP_con[0, 0])
+            ObjRegret2[sim, episode] = abs(val_k[0, 0] - opt_value_LP_con[0, 0]) #r = v_pi_k(P) - v_pi*(P)
             ConRegret2[sim, episode] = max(0, cost_k[0, 0] - CONSTRAINT)
             objs.append(ObjRegret2[sim, episode])
             cons.append(ConRegret2[sim, episode])
             if cost_k[0, 0] > CONSTRAINT:
-                NUMBER_INFEASIBILITIES[sim, episode] = 1
+                NUMBER_INFEASIBILITIES[sim, episode] = 1 #no solution, not a safe policy
         else:
             ObjRegret2[sim, episode] = ObjRegret2[sim, episode - 1] + abs(val_k[0, 0] - opt_value_LP_con[0, 0])
             ConRegret2[sim, episode] = ConRegret2[sim, episode - 1] + max(0, cost_k[0, 0] - CONSTRAINT)
@@ -109,6 +127,7 @@ for sim in range(NUMBER_SIMULATIONS):
             if cost_k[0, 0] > CONSTRAINT:
                 NUMBER_INFEASIBILITIES[sim, episode] = NUMBER_INFEASIBILITIES[sim, episode - 1] + 1
         
+        #reset every episode
         ep_count = np.zeros((N_STATES, N_STATES))
         ep_count_p = np.zeros((N_STATES, N_STATES, N_STATES))
 
@@ -118,26 +137,22 @@ for sim in range(NUMBER_SIMULATIONS):
             #if sum(prob) != 1:
             #    print(s, h)
             #    print(prob)
-            a = int(np.random.choice(STATES, 1, replace = True, p = prob))
+            a = int(np.random.choice(STATES, 1, replace = True, p = prob)) #select action randomly according to p
             next_state, rew, cost = util_methods.step(s, a, h)
-            ep_count[s, a] += 1
-            ep_count_p[s, a, next_state] += 1
+            ep_count[s, a] += 1 #update I(s,a)
+            ep_count_p[s, a, next_state] += 1 #update I(s,a,s')
             s = next_state
-        if episode != 0 and episode%50000== 0:
-
+        if episode != 0 and (episode+1)%5000 == 0: #decrease from 5e4 to 5e3 for better debug
+            duration = time.time() - start_time
+            print('Epoch {:d} Duration {:.3f} seconds.'.format(episode + 1, duration))
             filename = 'opsrl-in' + str(RUN_NUMBER) + '.pckl'
             f = open(filename, 'ab')
             pickle.dump([NUMBER_SIMULATIONS, NUMBER_EPISODES, objs , cons, pi_k, NUMBER_INFEASIBILITIES, q_k], f)
             f.close()
             objs = []
             cons = []
-        elif episode == NUMBER_EPISODES-1:
-            filename = 'opsrl-in' + str(RUN_NUMBER) + '.pckl'
-            f = open(filename, 'ab')
-            pickle.dump([NUMBER_SIMULATIONS, NUMBER_EPISODES, objs , cons, pi_k, NUMBER_INFEASIBILITIES, q_k], f)
-            f.close()
         
-
+print('Finished Training.')
 ObjRegret_mean = np.mean(ObjRegret2, axis = 0)
 ConRegret_mean = np.mean(ConRegret2, axis = 0)
 ObjRegret_std = np.std(ObjRegret2, axis = 0)
--- Codes/InventoryControl/model-in.py
+++ Codes/InventoryControl/model-in.py
@@ -17,38 +17,45 @@ import pulp as p
 import math
 from copy import copy
 
-
+#fixed cost when the inventory is brought in
 K = 4
+
+#holding cost s for storing inventory
 def h(x):
     return x
 
+#revenue function
 def f(x):
     if x > 0:
         return 8*x/100
     return 0
 
+#purchase cost when inventory is brought in
 def O(x):
     if x > 0:
         return K + 2*x
     return 0
 
-N_STATES = 6
-actions = {}
+N_STATES = 6 #number of states, namely maximum capacity for storage
+actions = {} #actions the agent can take
 
-delta = 0.01
+delta = 0.01 #constant used in formula
 
+R = {} #reward
+C = {} #cost
+P = {} #probability of the model
 
-R = {}
-C = {}
-P = {}
+demand = [0.3,0.2,0.2,0.2,0.05,0.05] #distribution of stochastic exogenous demand, range from 0 to 5
 
-demand = [0.3,0.2,0.2,0.2,0.05,0.05]
+suffix = '-------------' #suffix for display concern
 
+#permissible actions for each state s, i.e. actions[0] = [0,1,2,3,4,5], an agent can buy 0-5 items since no storage used 
 for s in range(N_STATES):
     actions[s] = []
     for a in range(N_STATES - s):
         actions[s].append(a)
 
+#compute cost, p
 for s in range(N_STATES):
     l = len(actions[s])
     R[s] = np.zeros(l)
@@ -61,8 +68,9 @@ for s in range(N_STATES):
             s_ = s + a - d
             if s_ < 0:
                 s_ = 0
-            elif s_ > N_STATES - 1:
-                s_ = N_STATES - 1
+            # will never happen, max(s_) = max(a + s) - min(d) = N_STATES - 1 - 0
+            # elif s_ > N_STATES - 1: 
+            #    s_ = N_STATES - 1
                 
             P[s][a][s_] += demand[d]
         R[s][a] = 0
@@ -72,27 +80,31 @@ for s in range(N_STATES):
 for s in range(N_STATES):
     for a in actions[s]:        
       for d in range(N_STATES):
-            s_ = min(max(0,s+a-d),N_STATES-1)
+            s_ = max(0,s+a-d) #ditto, no min needs -> s = max(0, s + a - d)
             if s + a - d >= 0:
                 R[s][a] += P[s][a][s_]*f(d)
             else:
                 R[s][a] += 0
             
+def print_P_matrix(): #print matrix P with better format
+    print(suffix + ' Print P Matrix ' + suffix)
+    for s in range(N_STATES):
+        for a in actions[s]:
+            print('[{}, {}]: {}'.format(s, a, P[s][a]))    
+print_P_matrix()
 
-
+#find r_max, c_max for normalization concern
 r_max = R[0][0]
 c_max = C[0][0]
-print(P)
-
-
 for s in range(N_STATES):
     for a in actions[s]:
         if C[s][a] > c_max:
             c_max = C[s][a]
         if R[s][a] > r_max:
             r_max = R[s][a]
+print(suffix + ' Max R ' + suffix + '\n', r_max)
 
-print(r_max)
+#normalize C and R
 for s in range(N_STATES):
     for a in actions[s]:
         C[s][a] = C[s][a]/c_max
@@ -102,31 +114,29 @@ EPISODE_LENGTH = 7
 
 CONSTRAINT = EPISODE_LENGTH/2
 
-C_b = CONSTRAINT/5  #Change this if you want different baseline policy.
+C_b = CONSTRAINT/5  #Constraint for safety baseline policy, change this if you want different baseline policy.
 
-NUMBER_EPISODES = 1e6
+NUMBER_EPISODES = 5e4 #cut run time in half
 NUMBER_SIMULATIONS = 1
-
 
 EPS = 0.01
 M = 0
 
-util_methods_1 = utils(EPS, delta, M, P,R,C,EPISODE_LENGTH,N_STATES,actions,CONSTRAINT,C_b)
+util_methods_1 = utils(EPS, delta, M, P,R,C,EPISODE_LENGTH,N_STATES,actions,CONSTRAINT,C_b) #finding optimal policy
 opt_policy_con, opt_value_LP_con, opt_cost_LP_con, opt_q_con = util_methods_1.compute_opt_LP_Constrained(0)
 opt_policy_uncon, opt_value_LP_uncon, opt_cost_LP_uncon, opt_q_uncon = util_methods_1.compute_opt_LP_Unconstrained(0)
+#save model
 f = open('solution-in.pckl', 'wb')
 pickle.dump([opt_policy_con, opt_value_LP_con, opt_cost_LP_con, opt_q_con, opt_policy_uncon, opt_value_LP_uncon, opt_cost_LP_uncon, opt_q_uncon], f)
 f.close()
 
 
-util_methods_1 = utils(EPS, delta, M, P,R,C,EPISODE_LENGTH,N_STATES,actions,C_b,C_b)
+util_methods_1 = utils(EPS, delta, M, P,R,C,EPISODE_LENGTH,N_STATES,actions,C_b,C_b) #set CONSTRAINT = C_b for finding safe baseline policy 
 policy_b, value_b, cost_b, q_b = util_methods_1.compute_opt_LP_Constrained(0)
+#save model
 f = open('base-in.pckl', 'wb')
 pickle.dump([policy_b, value_b, cost_b, q_b], f)
 f.close()
-
-
-
 
 f = open('model-in.pckl', 'wb')
 pickle.dump([NUMBER_SIMULATIONS, NUMBER_EPISODES, P, R, C, CONSTRAINT, N_STATES, actions, EPISODE_LENGTH, delta], f)
